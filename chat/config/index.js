702399a3fa5f72fbb87db6b1f56d0ded072c41a7
Katsiaryna Lunchuk <katsiarynalunchuk@mac-205s-Mac-mini.local>
2018-07-04 16:05:05 +0300

    Add sessipn package. Create new settings

--- chat/config/index.js
+++ chat/config/index.js
@@ -1,6 +1,9 @@
-const nconf = require('nconf'),
-    path = require('path');
+const nconf = require("nconf"),
+  path = require("path");
 
-nconf.argv().env().file({file: path.join(__dirname, 'config.json')});
+nconf
+  .argv()
+  .env()
+  .file({ file: path.join(__dirname, "config.js") });
 
 module.exports = nconf;
--- chat/lib/mongoose.js
+++ chat/lib/mongoose.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose'),
-    config = require('../config/config.json');
+import mongoose from "mongoose";
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/chat", {"options": {
-    "server": {
-        "socketOptions": {
-            "keepAlive": 1
+mongoose.connect(
+  "mongodb://localhost/chat",
+  {
+    options: {
+      server: {
+        socketOptions: {
+          keepAlive: 1
         }
-    }}
-});
+      }
+    }
+  }
+);
 
 module.exports = mongoose;
