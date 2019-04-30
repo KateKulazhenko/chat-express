d7f8b0f8e759cb2b14a6752007ce8335962365ba
Katsiaryna Lunchuk <katsiarynalunchuk@mac-205s-Mac-mini.local>
2019-04-30 18:51:02 +0300

    Update template

--- client/src/modules/Auth/views/SignUpForm/SignUpForm.tsx
+++ client/src/modules/Auth/views/SignUpForm/SignUpForm.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { reduxForm, InjectedFormProps, Field } from 'redux-form';
 import CustomInput from '../../../../components/Input';
+import Button from '@material-ui/core/Button';
 import Auth from '../..';
 
 interface Props {
@@ -11,6 +12,8 @@ const SignUpForm: React.StatelessComponent<Props> = ({ handleSubmit }) => {
   return <div>
     <form onSubmit={handleSubmit}>
       <Field name="lastName" component={CustomInput} label="Email" placeholder={'email@mail.com'} />
+      <Field name="password" component={CustomInput} label="Password" placeholder={'password'} />
+      <Button variant="outlined" color="primary">Sign In</Button>
     </form>
   </div>
 }
